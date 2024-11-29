ab60e4920ff6a19a8dcf7aa6b439cb2ec12bfd80
RenaldyNaufalTA <aldyreyn5@gmail.com>
2024-11-29 14:34:49 +0700

    update package

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        outDir: "public/build", // Ensure build output is in the right directory
+        rollupOptions: {
+            input: "resources/js/app.jsx",
+        },
+    },
 });
